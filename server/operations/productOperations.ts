280a9341f6cbe4b1147eed199f3958c87e5e3474
harpreetsinghkhattra <harpreetsinghkhattra@gmail.com>
2019-01-05 23:40:54 +0530

    Description: testing api on server

--- server/operations/productOperations.ts
+++ server/operations/productOperations.ts
@@ -207,7 +207,7 @@ export class ProductOperations {
                             isFollow: true
                         }, (err, data) => {
                             if (err) CommonJs.close(client, CommonJSInstance.ERROR, err, cb);
-                            else this.viewPortal({ userId: obj.id }, cb);
+                            else this.viewPortal({ userId: sellerId }, cb);
                         });
                     } else if (data && data.length > 0) {
                         const isFollow = data[0].isFollow ? false : true;
@@ -216,7 +216,7 @@ export class ProductOperations {
                             followedId: new ObjectId(sellerId)
                         }, { $set: { isFollow: isFollow } }, (err, data) => {
                             if (err) CommonJs.close(client, CommonJSInstance.ERROR, err, cb);
-                            else this.viewPortal({ userId: obj.id }, cb);
+                            else this.viewPortal({ userId: sellerId }, cb);
                         })
                     } else CommonJs.close(client, CommonJSInstance.NO_CHANGE, [], cb);
                 });
