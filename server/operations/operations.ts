5c283ff26fb10a4755173456f81d15bf2956af12
harpreetsinghkhattra <harpreetsinghkhattra@gmail.com>
2019-04-28 14:55:39 +0530

    Work on search api

--- server/operations/operations.ts
+++ server/operations/operations.ts
@@ -581,7 +581,7 @@ export class Operations {
                             business_address: 1,
                             isShown: {
                                 $and: [
-                                    { $ne: [{ $indexOfArray: [["designers \n(men, woman)", "garments", "boutiques"], "$category"] }, -1] }
+                                    { $ne: [{ $indexOfArray: [["multi brand's", "garments", "boutiques"], "$category"] }, -1] }
                                 ]
                             }
                         }
