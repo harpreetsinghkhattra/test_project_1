b299a20a1b6398a0d56d4d3f7f7e0ff4308dc8e4
harpreetsinghkhattra <harpreetsinghkhattra@gmail.com>
2019-05-20 22:35:34 +0530

    Work on notification issue

--- server/operations/productOperations.ts
+++ server/operations/productOperations.ts
@@ -113,7 +113,9 @@ export class ProductOperations {
                         else if (productData && productData.length) {
                             productData = productData[0];
                             const deviceTokens = data && data.length ? data.map(ele => ele.userData.deviceToken) : [];
-                            console.log(`${productData._id.str}`);
+                            console.log("${productData._id.str} ===> ", `${productData._id.str}`);
+                            console.log("${productData._id.str} ===> ", typeof `${productData._id}`);
+                            console.log("${productData._id.str} ===> ", typeof productData._id.valueOf());
                             if (deviceTokens && deviceTokens.length) {
                                 const message = {
                                     registration_ids: deviceTokens,
@@ -121,7 +123,7 @@ export class ProductOperations {
                                         title: productData && productData.name ? productData.name : "NA",
                                         description: productData && productData.description ? productData.description : "NA",
                                         image: images && images.length ? images[0] : "",
-                                        productId: `${productData._id.str}`,
+                                        productId: `${productData._id}`,
                                         notification_type: "product"
                                     }
                                 };
